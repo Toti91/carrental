b561cfd47711f00bb620fac9c425fc4fff8f9d34
Toti91 <torarinng@gmail.com>
2016-06-20 00:10:31 +0000

    Dashboard stats

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,20 @@
 	Sidebar
 @stop
 
+@section('title-bar')
+	<div class="content-5 title-bar-content">
+		<h3>Users</h3>
+	</div>
+	<div class="content-5 title-bar-content">
+		<h3>Cars</h3>
+	</div>
+@stop
+
 @section('container')
-	
+	<table class="stats-table">
+		<tr>
+			<td>{{ $users->count() }}</td>
+			<td>{{ $cars->count() }}</td>
+		</tr>
+	</table>
 @stop
